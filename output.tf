05b5dc6641760854b9898cbfa10ad8ae50863be4
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-02 16:45:46 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "load_balancer_dns_name" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.Nginx_alb.dns_name
+}
 output "balu_instance_public_ip" {
  description = "instance public ip of balu"
  value = aws_instance.Nginx_balu.public_ip
