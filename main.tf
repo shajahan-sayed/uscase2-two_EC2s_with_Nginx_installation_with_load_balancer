57a17133369df3cac4cd21959305cbdea0e31253
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-02 14:15:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -96,4 +96,78 @@ resource "aws_security_group" "Nginx2_sg2" {
   Name = "EC2-sg2"
  }
 }
+
+#creating EC2 instances
+resource "aws_instance" "Nginx_balu" {
+ ami_id = var.ami_id
+ instance_type = var.instance_type
+ key_name = var.key_name
+ subnet_id = aws_subnet.Nginx2_public_subnet1
+ vpc_security_group_id = [aws_security_group.Nginx2_sg2.id]
+
+ tags = {
+  Name = "Nginx_balu"
+ }
+}
+resource "aws_instance" "Nginx_shajahan" {
+ ami_id = var.ami_id
+ instance_type = var.instance_type
+ key_name = var.key_name
+ subnet_id = aws_subnet.Nginx2_public_subnet2
+ vpc_security_group_id = [aws_security_group.Nginx2_sg2.id]
+
+ tags = {
+  Name = "Nginx_shajahan"
+ }
+}
+
+#creating Load balancer (Application Load balancer)
+resourec "aws_lb" "Nginx_alb" {
+ name  = "Nginx_alb"
+ load_balancer_type = "application"
+ security_groups = [aws_security_group.Nginx2_sg2.id]
+ subnets = [ aws_subnet.Nginx2_public_subnet1
+             aws_subnet.Nginx2_public_subnet2]
+ tags = {
+  Name = "Nginx_alb"
+ }
+}
+
+#creating target group 
+resource "aws_lb_target_group" "Nginx_target" {
+ name = "Nginx_
+ port = 80
+ protocol = "HTTP"
+ vpc_id =  aws_vpc.Nginx2.id
+
+ tags = {
+  Name = "Nginx_target"
+ }
+
+#listerner
+resource "aws_lb_listener" "Nginx_listener" {
+ load_balancer_arn = aws_lb.Nginx_alb.arn
+ port = 80
+ protocol = "HTTP"
+
+ default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+#target group attachment to ec2
+resource "aws_lb_target_group_attachment" "balu_attach" {
+ target_group_arn = aws_lb_target_group.Nginx_alb.arn
+ target_id = aws_instance.Nginx_balu.id
+ port = 80
+}
+resource "aws_lb_target_group_attachment" "shajahan_attach" {
+ target_group_arn = aws_lb_target_group.Nginx_alb.arn
+ target_id = aws_instance.Nginx_shajahan.id
+ port = 80
+}
+ 
+
+ 
  
