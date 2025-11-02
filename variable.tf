c46ae3b3edf5c8ce23df8c1ad805cb66600c9a92
shajahan-sayed <shajahansayed111@gmail.com>
2025-11-02 15:57:26 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  description = "aws region to deploy resource"
+  type = string 
+}
+variable "instnace_type" {
+  description = "attaching instance type"
+  type = string
+}
+variable "ami_id" {
+  description = "attaching ami id "
+  type = string
+}
+variable "key_name" {
+  description = "attaching key name"
+  type = string
+}
+variable "vpc_Nginx2_cidr" {
+  description = "cidr block for vpc"
+  type = string
+}
+variable "Nginx2_cidr_pub1" {
+  description = "cidr block for subnet1"
+  type = string
+}
+variable "Nginx2_cidr_pub2" {
+  description = "cidr block for subnet2"
+  type = string
+}
